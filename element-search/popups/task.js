1f72b2488543e622fb2c95872e5cef08ee19d64a
nickupx <nick.upsky@gmail.com>
2020-07-25 16:42:33 +0300

    Update task.js

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -7,8 +7,8 @@ for (let i = 0; i < closers.length; i++) {
     }
 }
 
-document.getElementById('modal_main').classList.add('modal_active')
-
 document.getElementsByClassName('show-success')[0].onclick = function() {
     document.getElementById('modal_success').classList.add('modal_active')
 }
+
+document.getElementById('modal_main').classList.add('modal_active')
