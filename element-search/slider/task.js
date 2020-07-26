ca223cfbe51ff2ed5146f25aafcd990c4903ac60
nickupx <nick.upsky@gmail.com>
2020-07-26 15:36:09 +0300

    task 3 fixes

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -2,7 +2,6 @@ const slides = document.getElementsByClassName('slider__item')
 const dots = document.getElementsByClassName('slider__dot')
 
 function getActiveSlide() {
-    const slides = document.getElementsByClassName('slider__item')
     for (let i = 0; i < slides.length; i++)
         if (slides[i].classList.contains('slider__item_active')) {
             return i
@@ -12,25 +11,22 @@ function getActiveSlide() {
 function showSlide(slide) {
     document.querySelector('div.slider__item_active').classList.remove('slider__item_active')
     slides[slide].classList.add('slider__item_active')
+    switchDots()
 }
 
 function showNext() {
     if (getActiveSlide() === slides.length - 1) {
         showSlide(0)
-        switchDots()
     } else {
     showSlide(getActiveSlide() + 1)
-    switchDots()
     }
 }
 
 function showPrev() {
     if (getActiveSlide() === 0) {
         showSlide(slides.length - 1)
-        switchDots()
     } else {
     showSlide(getActiveSlide() - 1)
-    switchDots()
     }
 }
 
@@ -38,8 +34,8 @@ document.querySelector('div.slider__arrow_next').onclick = showNext
 document.querySelector('div.slider__arrow_prev').onclick = showPrev
 
 function renderDots(num) {
-    let dotsHtml = '<div class="slider__dot"></div>'
-    for (let i = 0; i < num - 1; i++) {
+    let dotsHtml = ''
+    for (let i = 0; i < num; i++) {
         dotsHtml += '<div class="slider__dot"></div>'
     }
     document.querySelector('div.slider__dots').innerHTML = dotsHtml   
@@ -47,7 +43,6 @@ function renderDots(num) {
     for (let i = 0; i < dots.length; i++) {
         dots[i].onclick = function() {
             showSlide(i)
-            switchDots()
         }
     }
 }
@@ -60,7 +55,6 @@ function switchDots() {
 }
 
 document.onkeydown = function(e) {
-    console.log(e)
     if (e.keyCode === 37) {
         showPrev()
     }
