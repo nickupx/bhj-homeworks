ec284a982ae8470b82a8d4751c481e3883286c8d
nickupx <nick.upsky@gmail.com>
2020-08-03 19:38:59 +0300

    multi-menu added

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,20 +1,16 @@
 const dropdowns = document.querySelectorAll('div.dropdown')
 const listItems = document.getElementsByClassName('dropdown__item')
 
-function toggleActive() {
-    document.querySelector('ul.dropdown__list').classList.toggle('dropdown__list_active')
-}
-
-for (let i = 0; i < dropdowns.length; i++) {
-    dropdowns[i].addEventListener('click', function() {
-        toggleActive()
+for (const dropdown of dropdowns) {
+    dropdown.addEventListener('click', function() {
+        this.querySelector('.dropdown__list').classList.toggle('dropdown__list_active')
         }
     )
 }
 
-for (let i =0; i < listItems.length; i++) {
-    listItems[i].addEventListener('click', function(e) {
+for (const listItem of listItems) {
+    listItem.addEventListener('click', function(e) {
         e.preventDefault()
-        document.querySelector('div.dropdown__value').textContent = listItems[i].querySelector('a.dropdown__link').textContent.replace(/\s/g, '')
+        this.closest('.dropdown__list').previousElementSibling.textContent = listItem.querySelector('.dropdown__link').textContent
     })
 }
