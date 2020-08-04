61107ba2308c364504ba0f80d8c2054c1d1d174c
nickupx <nick.upsky@gmail.com>
2020-08-04 19:25:06 +0300

    2 done

---
+++ html-forms/interests/task.js
@@ -0,0 +1,37 @@
+function countChecked(arr) {
+    let subCheckCount = 0
+    for (check of arr) {
+        if (check.checked) {
+            subCheckCount++
+        }
+    }
+    return subCheckCount
+}
+
+const arr = Array.from(document.querySelector('.interests_main').querySelectorAll('.interest__check'))
+
+for (item of arr) {
+    item.addEventListener('change', function(e) {
+        const subs = this.closest('label').nextElementSibling
+        const sibls = Array.from(this.closest('.interests').querySelectorAll('.interest__check'))
+        if (subs) {
+            const subChecks = Array.from(subs.querySelectorAll('.interest__check'))
+            if (this.checked) {
+                for (sub of subChecks) {
+                    sub.checked = true
+                }
+            } else {
+                for (sub of subChecks) {
+                    sub.checked = false
+                    }
+                }
+        } else {
+            if (!countChecked(sibls)) {
+                this.closest('.interests').parentElement.firstElementChild.querySelector('.interest__check').checked = false
+            } else if (countChecked(sibls) === sibls.length) {
+                this.closest('.interests').parentElement.firstElementChild.querySelector('.interest__check').checked = true
+                }
+            }
+        }    
+    )
+}
