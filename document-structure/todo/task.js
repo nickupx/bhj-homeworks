1bf481af802890f0baeb60717c6cc791353b6c5a
nickupx <nick.upsky@gmail.com>
2020-08-05 22:07:47 +0300

    1 2 done

---
+++ document-structure/todo/task.js
@@ -0,0 +1,48 @@
+const form = document.querySelector('.tasks__control')
+const input = document.getElementById('task__input')
+
+let tasks
+if (localStorage.getItem('tasks')) {
+    tasks = JSON.parse(localStorage.getItem('tasks'))
+    renderTasks(tasks)
+} else {
+    tasks = []
+}
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault()
+    if (input.value) {
+        tasks.push(input.value)
+        renderTasks(tasks)
+        updateLocalStorage()
+        form.reset()
+    }
+})
+
+function renderTasks(tasks) {
+    let html = ''
+    for (let i = 0; i < tasks.length; i++) {
+        html += `
+            <div class="task" data-id="${i}">
+                <div class="task__title">
+                ${tasks[i]}
+                </div>
+                <a href="#" class="task__remove">&times;</a>
+            </div>
+        `
+    }
+    document.getElementById('tasks__list').innerHTML = html
+
+    const removeLinks = document.getElementsByClassName('task__remove')
+    for (link of removeLinks) {
+        link.addEventListener('click', function(e) {
+            this.parentElement.remove()
+            tasks.splice(this.parentElement.dataset.id, 1)
+            updateLocalStorage()
+        })
+    }
+}
+
+function updateLocalStorage() {
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+}
---
+++ document-structure/tooltip/task.js
@@ -0,0 +1,33 @@
+function showTooltip(e) {
+    e.preventDefault()
+    const query = document.querySelector('.tooltip')
+    const coords = this.getBoundingClientRect()
+    const hrefHeight = this.getBoundingClientRect().bottom - this.getBoundingClientRect().top
+    
+
+    if (query) {
+        if (this.getAttribute('title') === query.textContent) {
+            query.remove()
+            return
+        }
+        query.remove()
+    }
+    let tooltip = document.createElement('div')
+    console.log(tooltip)
+    console.log(this)
+    tooltip.classList.add('tooltip')
+    tooltip.style.cssText = 'left: 0; top: 0'
+    tooltip.style.display = 'block'
+    tooltip.style.top = coords.top + hrefHeight + 'px'
+    tooltip.style.left = coords.left + 'px'
+    tooltip.textContent = this.getAttribute('title')
+    document.body.append(tooltip)
+
+    //setTimeout(() => tooltip.remove(), 2000)
+}
+
+let hrefs = document.getElementsByClassName('has-tooltip')
+
+for (href of hrefs) {
+    href.addEventListener('click', showTooltip)
+}
