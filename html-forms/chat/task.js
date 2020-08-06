6d7377897909ebb55e1b1d29b16de62bdad6a9fa
nickupx <nick.upsky@gmail.com>
2020-08-06 22:30:05 +0300

    function refactor

--- html-forms/chat/task.js
+++ html-forms/chat/task.js
@@ -1,8 +1,8 @@
 const chatWidget = document.querySelector('.chat-widget')
 const container = document.querySelector('.chat-widget__messages')
-const messages = document.querySelector('.chat-widget__messages')
 const robotMessages = ['Да что ж вы за человек-то такой', 'Да что вы говорите?', 'Просто невероятно', 'Пришлите ваш запрос по факсу', 'Все операторы заняты']
 const input = document.getElementById('chat-widget__input')
+let lastActivity
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max))
@@ -10,59 +10,57 @@ function getRandomInt(max) {
 
 function getCurrentTime() {
     const date = new Date()
-    const dateNormal = `${date.getDay()}.${date.getMonth()}.${date.getFullYear()}, ${date.getHours()}:${date.getMinutes()}`
-    return dateNormal
+    const dateTime = `${date.getDay()}.${date.getMonth()}.${date.getFullYear()}, ${date.getHours()}:${date.getMinutes()}`
+    return dateTime
 }
 
-let lastActivity
+function scrollTo() {
+    container.parentElement.scrollTo(0, parseInt(getComputedStyle(container).height))
+ }
 
-chatWidget.addEventListener('click', (e) => {
+ function renderMessage(text, client) {
+     const message = document.createElement('div')
+     message.classList.add('message')
+     if (client) {
+         message.classList.add('message_client')
+     }
+     const msgTime = document.createElement('div')
+     msgTime.classList.add('message__time')
+     msgTime.textContent = getCurrentTime()
+     const msgText = document.createElement('div')
+     msgText.classList.add('message__text')
+     msgText.textContent = text
+     message.appendChild(msgTime)
+     message.appendChild(msgText)
+     container.appendChild(message)
+     scrollTo()
+     lastActivity = +new Date()
+ }
+
+ chatWidget.addEventListener('click', (e) => {
     chatWidget.classList.add('chat-widget_active')
     lastActivity = +new Date()
     }
 )
 
+const sendClientMsg = () => {
+    renderMessage(input.value, true)
+    input.value = ''
+    setTimeout(sendRobotMsg, 1000)
+}
+
+const sendRobotMsg = () => {
+    renderMessage(robotMessages[getRandomInt(robotMessages.length)], false)
+}
+
 input.addEventListener('keyup', function(e) {
     if (e.keyCode === 13 && input.value) {
-        sendClientMessage()
-        input.value = ''
+        sendClientMsg()
     }
 })
 
-function sendClientMessage() {
-    messages.innerHTML += `
-    <div class="message message_client">
-        <div class="message__time">${getCurrentTime()}</div>
-        <div class="message__text">${input.value}</div>
-    </div>
-    `
-    scrollTo()
-    setTimeout(sendRobotMessage, 2000)
-}
-
-function sendRobotMessage() {
-    messages.innerHTML += `
-    <div class="message">
-        <div class="message__time">${getCurrentTime()}</div>
-        <div class="message__text">${robotMessages[getRandomInt(robotMessages.length)]}</div>
-    </div>
-    `
-    scrollTo()
-}
-
-function scrollTo() {
-    container.parentElement.scrollTo(0, parseInt(getComputedStyle(container).height))
-    lastActivity = +new Date()
- }
-
 setInterval(() => {
     if (chatWidget.classList.contains('chat-widget_active') && +new Date() >= lastActivity + 30000) {
-        messages.innerHTML += `
-        <div class="message">
-            <div class="message__time">${getCurrentTime()}</div>
-            <div class="message__text">Больше ничего не хотите написать?</div>
-        </div>
-        `
-        scrollTo()
+        renderMessage('Вы не уснули?', false)
     }
 }, 1000)
